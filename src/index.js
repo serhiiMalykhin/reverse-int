bba9688dfbca230a19cb3f2f5eea955377f873f9
serhiiMalykhin <32640104+serhiiMalykhin@users.noreply.github.com>
2025-07-27 02:38:25 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-module.exports = function reverse(/* n */) {
-  throw new Error('Not implemented');
+module.exports = function reverse(n) {
+  let res='';
+  while(n!==0) {
+    let temp = n%10;
+    res += temp
+    n = Math.floor( n/10);
+  }
+  return Number(res);
 };
