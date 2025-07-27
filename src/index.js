9a21da4248d67cc856259dd1c123446f886cb2c1
serhiiMalykhin <32640104+serhiiMalykhin@users.noreply.github.com>
2025-07-27 20:07:47 +0300

    fix: code index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,3 @@
 module.exports = function reverse(n) {
-  let res='';
-  while(n!==0) {
-    let temp = n%10;
-    res += temp
-    n = Math.floor( n/10);
-  }
-  return Number(res);
+  return Number(String(Math.abs(n)).split('').reverse().join(''));
 };
